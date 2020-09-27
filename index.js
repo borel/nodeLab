8bd0e0d2c8a5ed1d3fab94d01adf04f2f2eda0b1
pbborel <pbborel@vente-privee.com>
2020-09-27 15:32:02 +0200

    Add game

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// READY
+const isDivideBy = (number, a, b) => {
+  return Number.isInteger(number / a) && Number.isInteger(number / b);
+};
+
+const isDivideBy = (number, a, b) => {
+  return [a, b].every((n) => Number.isInteger(number / n));
+};
+
+console.log("res", isDivideBy(-12, 2, -6));
+console.log("res", isDivideBy(45, 1, 6));
+console.log("res", isDivideBy(1, 1, "a"));
