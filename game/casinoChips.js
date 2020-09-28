f021b3f8ec489b1973aa12b14183646aa8e75a49
pbborel <pbborel@vente-privee.com>
2020-09-28 23:11:10 +0200

    Add game

---
+++ game/casinoChips.js
@@ -0,0 +1,6 @@
+const solve = (array) => {
+    const [min, mid, max] = array.sort((a, b) => a - b);
+    return min + mid > max ? max + Math.floor((min + mid - max) / 2) : min + mid;
+  };
+  
+  console.log(solve([8, 9, 12]));
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const isDivideBy = (number, a, b) => {
-  return Number.isInteger(number / a) && Number.isInteger(number / b);
+const solve = (array) => {
+  const [min, mid, max] = array.sort((a, b) => a - b);
+  return min + mid > max ? max + Math.floor((min + mid - max) / 2) : min + mid;
 };
 
-const isDivideBy = (number, a, b) => {
-  return [a, b].every((n) => Number.isInteger(number / n));
-};
-
-console.log("res", isDivideBy(-12, 2, -6));
-console.log("res", isDivideBy(45, 1, 6));
-console.log("res", isDivideBy(1, 1, "a"));
+console.log(solve([8, 9, 12]));
